12eb3bb31f51bba6b07bb2ba34d6fb9f76a7dea7
Nada Hamaoud Almalaq <444200899@student.ksu.edu.sa>
2026-04-30 22:48:38 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,61 +1,61 @@
-// scripts.js
-
-function addLoadMessage() {
+function addLoadMessage(){
   var footer = document.getElementsByTagName("footer")[0];
 
+
+  
   if (footer) {
-    var newP = document.createElement("p");
-    var today = new Date();
-    var text = document.createTextNode("Page loaded on: " + today);
-
+    var newP =document.createElement("p");
+    var today= new Date() ;
+    var text =document.createTextNode("Page loaded on: " + today);
     newP.appendChild(text);
-    footer.appendChild(newP);
-  }
-}
+    footer.appendChild(newP); }}
 
 
-function validateForm() {
-  var name = document.getElementById("name");
-  var email = document.getElementById("email");
 
-  if (name.value == "" || email.value == "") {
+
+function validateForm(){
+  var name=document.getElementById("name");
+  var email =document.getElementById("email");
+
+  
+  if (name.value == "" ||email.value== ""){
+    
     alert("Please fill out all required fields.");
-    return false;
-  } else {
-    return true;
-  }
+    return false;}
+  else{return true;}
+
 }
 
+function toggleDescription(id){
+  var desc =document.getElementById(id);
 
-function toggleDescription(id) {
-  var desc = document.getElementById(id);
+  
+  if(desc.style.display == "none"){
+    desc.style.display = "block";}
+  else {desc.style.display = "none";
+  }}
 
-  if (desc.style.display == "none") {
-    desc.style.display = "block";
-  } else {
-    desc.style.display = "none";
-  }
-}
+function quoteEvents(){
+  var quote =document.getElementById("quote");
 
-
-function quoteEvents() {
-  var quote = document.getElementById("quote");
-
-  if (quote) {
-    quote.addEventListener("mouseover", function() {
+  if (quote){
+    
+    quote.addEventListener("mouseover", function(){
+      
       quote.style.backgroundColor = "#fff7cc";
       quote.style.fontStyle = "italic";
     });
 
-    quote.addEventListener("mouseout", function() {
+    quote.addEventListener("mouseout", function(){
       quote.style.backgroundColor = "";
       quote.style.fontStyle = "";
-    });
-  }
+    }); }
+
+
 }
 
+window.onload =function(){
+  if (document.body.id== "home"){
+    addLoadMessage(); }
 
-window.onload = function() {
-  addLoadMessage();
-  quoteEvents();
-};
+  quoteEvents(); };
